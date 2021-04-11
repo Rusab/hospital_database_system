d0efe6493580b96784670c2a2a5d7f2041510f5d
Rusab <rusabsarmun@gmail.com>
2021-04-11 22:57:22 +0600

    add delete function and view single entry

--- dbconnect.py
+++ dbconnect.py
@@ -28,16 +28,32 @@ def quote_str(text):
 
 def display_all(table_name):
     con, cur = connect_db("HospitalDatabase")
-
-    
+   
     cur.execute("SELECT * from {0};".format(table_name))
     
-    data =cur.fetchall()
-    
-
+    data =cur.fetchall() 
     print(data)
+    
+    disconnect_db(con, cur)
+    
+def view_entry(table_name, field_name, data):    
+    con, cur = connect_db("HospitalDatabase")
+    
+    try:
+        cur.execute("SELECT * from {table} WHERE {field} = {data};".format(table = table_name, field = field_name, data = data))
+        row = cur.fetchone() 
+        
+        for (entry, field) in zip(row, table_fields[table_name]):
+            print(field, entry)
 
+        # for field in table_fields[table_name] :
+        #     print(field)
 
+  
+    except:
+        print("Couldn't find entry")
+    
+        
     disconnect_db(con, cur)
 
 def insert_data(table_name, name, expertise, degree, position, chamber, time, fee, contactno, ids):
@@ -50,8 +66,18 @@ def insert_data(table_name, name, expertise, degree, position, chamber, time, fe
 
     disconnect_db(con, cur)
     
+def delete_data(table_name, field_name, data):
+    con, cur = connect_db("HospitalDatabase")
     
-
+    cur = con.cursor()
+    
+    try:
+        cur.execute("DELETE FROM {table} WHERE {field} = {data}".format(table = table_name, field = field_name, data=  data))       
+        con.commit()
+    except:
+        print("Data not found")
+        
+    disconnect_db(con, cur)
 
 # name = quote_str('Dragon We')
 # expertise = "'Cardiology'"
@@ -64,19 +90,55 @@ def insert_data(table_name, name, expertise, degree, position, chamber, time, fe
 # ids = 5
 
 
+table_fields = {'doctorsmanagement' : ["Doctor Name: ", "Expertise: ", "Degree: ", "Position: ", "Chamber: ", "Time: ", "Fee: ", "Contact no: ", "id: "]}
 
-name = quote_str(input("Doctor Name: "))
-expertise = quote_str(input("Area of expertise: "))
-position = quote_str(input("Position: "))
-chamber = quote_str(input("Chamber Location: "))
-degree = quote_str(input("Degree: "))
-time = quote_str(input("Availble at: "))
-fee = input("Fee: ")
-contactno = quote_str( input("Phone number: "))
-ids = 1
 
-insert_data('doctorsmanagement', name, expertise, degree, position, chamber, time, fee, contactno, ids)
-display_all('doctorsmanagement')
+
+
+
+while(True):
+    
+    print("\nWelcome to Panda Hospital Database \n What would you like to do:")
+    print("1. Add new entry\n2. See all entries\n3. Delete entry\n4.View entry\n5.Cancel\n")
+    x = int(input())
+    
+    if x == 1:
+        print("Enter the following data: \n")
+        name = quote_str(input("Doctor Name: "))
+        expertise = quote_str(input("Area of expertise: "))
+        position = quote_str(input("Position: "))
+        chamber = quote_str(input("Chamber Location: "))
+        degree = quote_str(input("Degree: "))
+        time = quote_str(input("Availble at: "))
+        fee = input("Fee: ")
+        contactno = quote_str( input("Phone number: "))
+        ids = 1
+        
+        insert_data('doctorsmanagement', name, expertise, degree, position, chamber, time, fee, contactno, ids) 
+        
+    elif x == 2:
+        print("Displaying all data: ")
+        
+        display_all('doctorsmanagement') 
+    
+    elif x == 3:
+        field_name = input("Choose a field: ")
+        data = input("Enter data: ")
+        
+        delete_data('doctorsmanagement', field_name, data)   
+        
+    elif x == 4:
+        field_name = input("Enter field: ")
+        data = quote_str(input("Enter {field}: ".format(field = field_name)))
+        view_entry('doctorsmanagement', field_name, data)  
+    
+    else: 
+        break
+
+
+
+#insert_data('doctorsmanagement', name, expertise, degree, position, chamber, time, fee, contactno, ids)
+
 
 
 
